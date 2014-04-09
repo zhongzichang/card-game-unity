55591f35c7777f9ce0c6df910b83fa93cb627071
zhong zichang <zhongzichang@gmail.com>
2014-04-09 16:48:10 +0800

    增加有向的寻路代理

---
+++ uc/Assets/TangScene/Scripts/Inner/DirectedNavAgent.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace TangScene {
+
+  public class DirectedNavAgent : MonoBehaviour {
+
+    // speed
+    public float speed;
+    // stopping distance
+    public float stoppingDistance;
+    // destination
+    public Vector3 destination{
+      private set;
+      get;
+    }
+
+    public bool hasPath {
+      private set;
+      get;
+    }
+
+
+
+  	// Use this for initialization
+  	void Start () {
+  	  
+  	}
+  	
+  	// Update is called once per frame
+  	void Update () {
+  	  
+  	}
+
+
+    public void ResetPath (){
+      hasPath = false;
+    }
+
+    public void SetDestination (Vector3 destination) {
+      this.destination = destination;
+      hasPath = true;
+    }
+    
+  }
+
+}
