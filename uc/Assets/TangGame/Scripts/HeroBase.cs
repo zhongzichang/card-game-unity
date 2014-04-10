9bd79432ec54ff7e90bb2c25a932fecfa958b248
xbhuang <130135189@qq.com>
2014-04-10 17:40:16 +0800

    move folder script to scriptes

---
+++ uc/Assets/TangGame/Scripts/HeroBase.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroBase{
+	private long id;
+	private int configId;
+	private string heroName;
+	private string heroAvatar;
+	private int level;
+	private HeroesRankEnum heroesRank;
+	private HeroPropertyEnum heroPropertyType;
+	private bool islock;
+	private int fragmentsCount;
+	private int fragmentsCountMax;
+	private HeroLocationEnum heroLocation;
+
+	public long Id {
+		get {
+			return id;
+		}
+		set {
+			id = value;
+		}
+	}
+
+	public HeroLocationEnum HeroLocation {
+		get {
+			return heroLocation;
+		}
+		set {
+			heroLocation = value;
+		}
+	}
+
+	public int ConfigId {
+		get {
+			return configId;
+		}
+		set{
+			configId = value;
+		}
+	}
+
+	public string HeroName {
+		get {
+			return heroName;
+		}
+		set{
+			heroName = value;
+		}
+	}
+
+	public int Level {
+		get {
+			return level;
+		}
+		set {
+			level = value;
+		}
+	}
+
+	public HeroesRankEnum HeroesRank {
+		get {
+			return heroesRank;
+		}
+		set {
+			heroesRank = value;
+		}
+	}
+
+	public string HeroAvatar {
+		get {
+			return heroAvatar;
+		}
+		set{
+			heroAvatar = value;
+		}
+	}
+
+	public HeroPropertyEnum HeroPropertyType {
+		get {
+			return heroPropertyType;
+		}
+		set{
+			heroPropertyType = value;
+		}
+	}
+
+	public bool Islock {
+		get {
+			return islock;
+		}
+		set{
+			islock = value;
+		}
+	}
+
+	public int FragmentsCount {
+		get {
+			return fragmentsCount;
+		}
+		set{
+			fragmentsCount = value;
+		}
+	}
+
+	public int FragmentsCountMax {
+		get {
+			return fragmentsCountMax;
+		}
+		set{
+			fragmentsCountMax = value;
+		}
+	}
+}
+
+public enum HeroPropertyEnum{
+	NONE,
+	STR,
+	INT,
+	AGI
+}
+public enum HeroLocationEnum{
+	NONE,
+	BEFORE,
+	MEDIUM,
+	LATER
+}
+public enum HeroesRankEnum{
+	NONE,
+	WHITE,
+	GREEN,
+	GREEN1,
+	BLUE,
+	BLUE1,
+	BLUE2,
+	PURPLE,
+	PURPLE1,
+	PURPLE2,
+	PURPLE3,
+	ORANGE
+
+}
---
+++ uc/Assets/TangGame/Scripts/MainPopupPanel.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using TangUI;
+
+public class MainPopupPanel : MonoBehaviour {
+
+	public GameObject MainPopupButton;
+	public GameObject HeroMenu;
+	public GameObject BagMenu;
+	public GameObject FragmentsMenu;
+	public GameObject TaskMenu;
+	public GameObject ActivityMenu;
+
+	private UIToggle mainPopupToggle;
+	// Use this for initialization
+	void Start () {
+		UIEventListener.Get (HeroMenu.gameObject).onClick += OnHeroMenuClick;
+		UIEventListener.Get (BagMenu.gameObject).onClick += OnBagMenuClick;
+		UIEventListener.Get (FragmentsMenu.gameObject).onClick += OnDebrisMenuClick;
+//		UIEventListener.Get (TaskMenu.gameObject).onClick += OnTaskMenuClick;
+
+		mainPopupToggle = MainPopupButton.GetComponent<UIToggle>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	// when hero menu click show hero panel
+	void OnHeroMenuClick(GameObject obj){
+		TangGame.UIContext.mgr.LazyOpen ("HeroViewPanel",UIPanelNode.OpenMode.ADDITIVE);
+		this.CloseMenu ();
+	}
+	void OnBagMenuClick(GameObject obj){
+		TangGame.UIContext.mgr.LazyOpen("WindowPanel" ,UIPanelNode.OpenMode.ADDITIVE);
+		this.CloseMenu ();
+	}
+	void OnDebrisMenuClick(GameObject obj){
+		TangGame.UIContext.mgr.LazyOpen ("QuestLogPanel" , UIPanelNode.OpenMode.REPLACE);
+		this.CloseMenu ();
+	}
+
+
+
+	// Close current Menu!
+	void CloseMenu(){
+		if (!mainPopupToggle.value) {
+			return;		
+		}
+		mainPopupToggle.value = false;
+		MainPopupButton.GetComponent<UIPlayTween> ().Play (true);
+	}
+}
