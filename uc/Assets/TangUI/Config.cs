3812ee147ce77995358675375e9a7ccdf0609b33
zhong zichang <zhongzichang@gmail.com>
2014-04-11 17:09:49 +0800

    调整UI配置，TangUI.Config 增加 WIDGET_PATH

--- uc/Assets/TangUI/Config.cs
+++ uc/Assets/TangUI/Config.cs
@@ -8,15 +8,19 @@ namespace TangUI
   public class Config
   {
     public const string PANEL_DIR = "Panels";
-
+    public const string WIDGET_DIR = "Widgets";
     public static bool use_packed_res = false;
 
-    public static string PANEL_PATH
-    {
-      get
-	{
-	  return  Tang.Config.U3D_PREFAB_DIR + Tang.Config.DIR_SEP + PANEL_DIR;
-	}
+    public static string PANEL_PATH {
+      get {
+        return  Tang.Config.U3D_PREFAB_DIR + Tang.Config.DIR_SEP + PANEL_DIR;
+      }
+    }
+
+    public static string WIDGET_PATH {
+      get {
+        return Tang.Config.U3D_PREFAB_DIR + Tang.Config.DIR_SEP + WIDGET_DIR;
+      }
     }
   }
 }
