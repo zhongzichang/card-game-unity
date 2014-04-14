b2bf2aeaa38de1f9c9f98ded782c1d622897013a
zhong zichang <zhongzichang@gmail.com>
2014-04-14 14:06:32 +0800

    remove static field baseDepth from UIPanelNodeManager

--- uc/Assets/TangUI/UIPanelNodeManager.cs
+++ uc/Assets/TangUI/UIPanelNodeManager.cs
@@ -10,7 +10,6 @@ namespace TangUI
   public class UIPanelNodeManager
   {
 
-    private static int baseDepth = 0;
     private UIPanelNodeContext context;
 
     public UIPanelNodeManager (UIAnchor anchor)
@@ -23,8 +22,6 @@ namespace TangUI
       this.context.currentNode = root;
       this.context.cache = new UIPanelCache ();
       this.context.anchor = anchor;
-      //this.context.depth = baseDepth;
-      //baseDepth += 1000;
     }
 
     public void LazyOpen (string name)
