31d5e41201f144c0eedce825c433e99a17b3b515
lite <litekok@gmail.com>
2014-04-18 19:02:37 +0800

    word UI and battle UI

---
+++ uc/Assets/TangUI/test/UiStatusTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TangUI
+{
+	public class UiStatusTest : MonoBehaviour
+	{
+
+		UIAnchor anchor;
+		UIPanelNodeManager mgr;
+
+		// Use this for initialization
+		void Start ()
+		{
+
+			anchor = GetComponent<UIAnchor> ();
+
+			if (anchor != null) {
+				mgr = new UIPanelNodeManager (anchor);
+				mgr.LazyOpen (TangGame.UIContext.MAIN_STATUS_PANEL_NAME, UIPanelNode.OpenMode.ADDITIVE, "status");
+			}
+		}
+
+	}
+}
+
