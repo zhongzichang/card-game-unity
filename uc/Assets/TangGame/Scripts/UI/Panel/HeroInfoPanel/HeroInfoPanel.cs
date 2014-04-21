2bae6e6fd71729cc30eaa447e305f9d383065beb
xbhuang <xbhuang1994@gmail.com>
2014-04-21 15:01:41 +0800

    完成技能的tip

--- uc/Assets/TangGame/Scripts/UI/Panel/HeroInfoPanel/HeroInfoPanel.cs
+++ uc/Assets/TangGame/Scripts/UI/Panel/HeroInfoPanel/HeroInfoPanel.cs
@@ -12,20 +12,19 @@ namespace TangGame
 		public GameObject SkillInfoSubPanel;
 		private HeroBase herobase;
 		// Use this for initialization
-		void Start ()
-		{
-			this.transform.localPosition += 100 * Vector3.left;
+		void Awake(){
 			DynamicBindUtil.BindScriptAndProperty (InterfaceSubPanel, InterfaceSubPanel.name);
 			DynamicBindUtil.BindScriptAndProperty (PictorialSubPanel, PictorialSubPanel.name);
 			DynamicBindUtil.BindScriptAndProperty (PropertyInfoSubPanel, PropertyInfoSubPanel.name);
 			DynamicBindUtil.BindScriptAndProperty (SkillInfoSubPanel, SkillInfoSubPanel.name);
-			herobase = param as HeroBase;
-			this.RefreshSubPanel (herobase);
+		}
+		void Start ()
+		{
+
 		}
 
 		void OnEnable ()
 		{
-			this.transform.localPosition += 100 * Vector3.left;
 			if (herobase != param as HeroBase) {
 				herobase = param as HeroBase;
 				this.RefreshSubPanel (herobase);
