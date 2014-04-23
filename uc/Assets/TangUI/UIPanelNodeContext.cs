737b2a54f5cde847ebbcc9a4d374d9125ea14b3a
zhong zichang <zhongzichang@gmail.com>
2014-04-23 18:16:52 +0800

    恢复UI面板初始深度为0

--- uc/Assets/TangUI/UIPanelNodeContext.cs
+++ uc/Assets/TangUI/UIPanelNodeContext.cs
@@ -9,8 +9,7 @@ namespace TangUI
   public class UIPanelNodeContext
   {
     public UIPanelCache cache;
-    // 初始深度设置为 -2 ，0 保留给场景对象使用
-    public int depth = -2;
+    public int depth = 0;
     public UIAnchor anchor;
     private UIPanelNode m_currentNode;
 
