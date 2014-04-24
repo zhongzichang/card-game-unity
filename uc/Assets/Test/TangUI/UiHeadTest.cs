c2676331d1289ce90b9d6103987a052e1dbaf17a
zhong zichang <zhongzichang@gmail.com>
2014-04-24 12:34:44 +0800

    调整UIpanel层次自适应，使用 RQ

---
+++ uc/Assets/Test/TangUI/UiHeadTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TangUI
+{
+	public class UiHeadTest : MonoBehaviour
+	{
+
+		UIAnchor anchor;
+		UIPanelNodeManager mgr;
+
+		// Use this for initialization
+		void Start ()
+		{
+
+			anchor = GetComponent<UIAnchor> ();
+
+			if (anchor != null) {
+				mgr = new UIPanelNodeManager (anchor);
+				mgr.LazyOpen (TangGame.UIContext.MAIN_HEAD_PANEL_NAME, UIPanelNode.OpenMode.ADDITIVE, "head");
+			}
+		}
+
+	}
+}
+
---
+++ uc/Assets/Test/TangUI/UifwTest.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TangUI
+{
+	public class UifwTest : MonoBehaviour
+	{
+
+		UIAnchor anchor;
+		UIPanelNodeManager mgr;
+
+		// Use this for initialization
+		void Start ()
+		{
+
+			anchor = GetComponent<UIAnchor> ();
+
+			if (anchor != null) {
+				mgr = new UIPanelNodeManager (anchor);
+				mgr.LazyOpen (TangGame.UIContext.MAIN_POPUP_PANEL_NAME, UIPanelNode.OpenMode.ADDITIVE, "test param");
+			}
+
+  
+		}
+  
+		void OnGUI ()
+		{
+			if (GUI.Button (new Rect (10, 10, 150, 100), "Back")) {
+
+				if (mgr != null) {
+					TangGame.UIContext.mgrCoC.Back ();
+				}
+
+			}
+
+//			if (GUI.Button (new Rect (200, 10, 150, 100), "New Main Popup Panel")) {
+//				if (mgr != null) {
+//					mgr.LazyOpen (TangGame.UIContext.MAIN_POPUP_PANEL_NAME, UIPanelNode.OpenMode.ADDITIVE, "test param");
+//				}
+//			}
+
+		}
+
+	}
+}
+
