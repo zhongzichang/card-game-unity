30e036fcdaf0600988ce4dd0fa4372fa3aa68f61
xbhuang <xbhuang1994@gmail.com>
2014-04-30 17:52:43 +0800

    提交进化表，修改props解析

---
+++ uc/Assets/TangGame/Scripts/Xml/EvolveXml.cs
@@ -0,0 +1,29 @@
+﻿using TangUtils;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+namespace TangGame.Xml
+{
+	public class EvolveXml
+	{
+		public int lv;
+		public int val;
+	}
+
+	[XmlRoot ("root")]
+	[XmlLate("evolve")]
+	public class EvolveRoot
+	{
+		[XmlElement ("value")]
+		public List<EvolveXml> items = new List<EvolveXml> ();
+
+		public static void LateProcess (object obj)
+		{
+			EvolveRoot root = obj as EvolveRoot;
+			foreach (EvolveXml item in root.items) {
+				Config.evolveXmlTable [item.lv] = item;
+			}
+		}
+	}
+}
