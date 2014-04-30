ec7d5aeda65d43148532791d2826749c277401cc
xbhuang <xbhuang1994@gmail.com>
2014-04-30 17:51:15 +0800

    提交进化表，修改props解析

--- uc/Assets/TangGame/Scripts/Config/Config.cs
+++ uc/Assets/TangGame/Scripts/Config/Config.cs
@@ -7,50 +7,59 @@ namespace TangGame
 {
 	public class Config
 	{
-
-    // XML 配置 ------
-    // 所有加载后的XML对象
-    public static Dictionary<string, object> xmlObjTable = new Dictionary<string, object> ();
-
+		// XML 配置 ------
+		// 所有加载后的XML对象
+		public static Dictionary<string, object> xmlObjTable = new Dictionary<string, object> ();
 		public static Dictionary<int,HeroXml> heroXmlTable = new Dictionary<int, HeroXml> ();
-
-		public static Dictionary<int,LevelUpXml> levelUpXmlTable = new Dictionary<int,LevelUpXml>();
-
+		public static Dictionary<int,LevelUpXml> levelUpXmlTable = new Dictionary<int,LevelUpXml> ();
 		public static Dictionary<int,SkillXml> skillXmlTable = new Dictionary<int, SkillXml> ();
-
-		public static Dictionary<int, PropsXml> propsXmlTable = new Dictionary<int, PropsXml>();
-
-
-
+		public static Dictionary<int, PropsXml> propsXmlTable = new Dictionary<int, PropsXml> ();
+		public static Dictionary<int,EvolveXml> evolveXmlTable = new Dictionary<int, EvolveXml> ();
 		/// <summary>
 		/// The properties heroes relationship.
 		/// 道具和英雄之间的关联
 		/// </summary>
-		public static Dictionary <int, ArrayList> propsHeroesRelationship = new Dictionary<int, ArrayList>();
+		public static Dictionary <int, ArrayList> propsHeroesRelationship = new Dictionary<int, ArrayList> ();
 		/// <summary>
 		/// The properties levels relationship.
 		/// 道具和关卡之间的关联
 		/// </summary>
-		public static  Dictionary <int, ArrayList> propsLevelsRelationship = new Dictionary<int, ArrayList>();
+		public static  Dictionary <int, ArrayList> propsLevelsRelationship = new Dictionary<int, ArrayList> ();
 		/// <summary>
 		/// The properties properties relationship.
 		/// 道具和道具之间的关联
 		/// 可合成的道具列表
 		/// </summary>
-		public static Dictionary <int, ArrayList> propsPropsRelationship = new Dictionary<int, ArrayList>();
+		public static Dictionary <int, ArrayList> propsPropsRelationship = new Dictionary<int, ArrayList> ();
 
 		/// <summary>
 		/// Adds the properties properties relationship.
 		/// </summary>
 		/// <param name="propsId1">Properties id1.</param>
 		/// <param name="propsId2">Properties id2.</param>
-		public static void addPropsPropsRelationship(int propsId1, int propsId2){
-			if (!propsPropsRelationship.ContainsKey (propsId1)) {
-				propsPropsRelationship.Add (propsId1, new ArrayList ());
+		public static void addPropsPropsRelationship (int propsXmlId1, int propsXmlId2)
+		{
+			if (!propsPropsRelationship.ContainsKey (propsXmlId1)) {
+				propsPropsRelationship.Add (propsXmlId1, new ArrayList ());
 			}
-			ArrayList array = propsPropsRelationship [propsId1];
-			if(!array.Contains(propsId2))
-				array.Add (propsId2);
+			ArrayList array = propsPropsRelationship [propsXmlId1];
+			if (!array.Contains (propsXmlId2))
+				array.Add (propsXmlId2);
+		}
+
+		/// <summary>
+		/// Adds the properties heroes relationship.
+		/// </summary>
+		/// <param name="propsXmlId">Properties xml identifier. 道具id</param>
+		/// <param name="heroXmlId">Hero xml identifier. 英雄id</param>
+		public static void addPropsHeroesRelationship (int propsXmlId, int heroXmlId)
+		{
+			if (!propsPropsRelationship.ContainsKey (propsXmlId)) {
+				propsPropsRelationship.Add (propsXmlId, new ArrayList ());
+			}
+			ArrayList array = propsPropsRelationship [propsXmlId];
+			if (!array.Contains (heroXmlId))
+				array.Add (heroXmlId);
 		}
 	}
 }
