02272439aadb3b30fcc860920ccbe2e8338717b3
hgq <hgq@temp.com>
2014-05-08 14:55:58 +0800

    1.战斗的英雄信息界面

---
+++ uc/Assets/TangGame/Scripts/UI/Panel/BattleHeroPanel.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TangGame{
+	
+	/// 战斗界面英雄信息面板
+	public class BattleHeroPanel : MonoBehaviour {
+		public UISprite background;
+		public BattleHeroItem heroItem;
+
+		public UIEventListener hpBtn;
+		public UIEventListener mpBtn;
+
+		private int hp;
+		private int mp;
+
+		void Start(){
+			//heroItem.gameObject.SetActive(false);
+			hpBtn.onClick += HpBtnClickHandler;
+			mpBtn.onClick += MpBtnClickHandler;
+		}
+
+		private void HpBtnClickHandler(GameObject go){
+			int temp = Random.Range(1,1000);
+			heroItem.SetHp(temp, 1000);
+		}
+
+		private void MpBtnClickHandler(GameObject go){
+			int temp = Random.Range(1,1000);
+			heroItem.SetMp(temp, 1000);
+		}
+
+	}
+}
