8479722d1c816a194a64d9489f980a9a9ee5036a
lite <litekok@gmail.com>
2014-05-08 19:00:15 +0800

    战役地图更多关卡信息

---
+++ uc/Assets/TangGame/Scenes/SanguoDemo/ChapterItemObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChapterItemObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
