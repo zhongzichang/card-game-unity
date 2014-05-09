4457c6c9a72a901c6ca3113c3246aaade91272d2
zhong zichang <zhongzichang@gmail.com>
2014-05-09 17:34:28 +0800

    大招进行中

---
+++ uc/Assets/TangLevel/Bhvr/Special/Skill/NativePlaceOnceBhvr.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TangLevel
+{
+  public class NativePlaceOnceBhvr : SkillSpecialBhvr
+  {
+    private Transform myTransform;
+
+    void Awake ()
+    {
+      myTransform = transform;
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+      if (isPlay) {
+	
+        if (!animation.isPlaying) {
+          GobjManager.Release (gameObject);
+        }
+
+      }
+    }
+
+    void OnEnable ()
+    {
+
+      if (w != null && w.target != null) {
+
+        // 绑定到目标身上
+        myTransform.localPosition = w.target.transform.localPosition;
+
+      } else {
+        GobjManager.Release (gameObject);
+      }
+
+      // 关卡控制
+      LevelController.RaisePause += OnPause;
+      LevelController.RaiseResume += OnResume;
+    }
+
+    public override void Play ()
+    {
+      isPlay = true;
+      animation.Play ();
+    }
+
+    public override void Pause ()
+    {
+      isPlay = false;
+      foreach (AnimationState state in animation) {
+        state.speed = 0;
+      }
+    }
+
+    public override void Resume ()
+    {
+      isPlay = true;
+      foreach (AnimationState state in animation) {
+        state.speed = 1;
+      }
+    }
+  }
+}
