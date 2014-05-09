1762b81a8eaf45328b20e98151423d0ead35342e
lite <litekok@gmail.com>
2014-05-09 16:53:04 +0800

    章节用ChapterItemObject和ChapterItemData

---
+++ uc/Assets/TangGame/Scenes/SanguoDemo/ChapterItemData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.Collections.Generic;
+
+public class ChapterItemData {
+  public string id;
+  public Hashtable stages;
+  public int minLevel;
+}
+
--- uc/Assets/TangGame/Scenes/SanguoDemo/ChapterItemObject.cs
+++ uc/Assets/TangGame/Scenes/SanguoDemo/ChapterItemObject.cs
@@ -1,9 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using System.Collections.Generic;
+
 public class ChapterItemObject : MonoBehaviour {
 
-	// Use this for initialization
+  public BetterList<StageItemObject> stages;
+  /// <summary>
+  /// 标题
+  /// </summary>
+  public UISprite title;
+  /// <summary>
+  /// 路径
+  /// </summary>
+  public UISprite path;
+  /// <summary>
+  /// 背景
+  /// </summary>
+  public UISprite background;
+
+  private ChapterItemData data;
+
+  // Use this for initialization
 	void Start () {
 	
 	}
