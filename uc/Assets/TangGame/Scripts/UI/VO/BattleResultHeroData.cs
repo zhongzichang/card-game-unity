76a14ee2cba03ca78950c97dca1bcc9e840571ec
hgq <hgq@temp.com>
2014-05-12 19:24:57 +0800

    1.战斗结果界面 2.战斗结果的数据界面

---
+++ uc/Assets/TangGame/Scripts/UI/VO/BattleResultHeroData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+namespace TangGame.UI{
+	/// 战斗结果面板英雄数据
+	public class BattleResultHeroData {
+		/// ID
+		public int id;
+		/// 等级
+		public int level;
+		/// 当前经验
+		public int exp;
+		/// 是否升级
+		public bool levelUp;
+		/// 输出伤害
+		public int damage;
+		/// 战斗中的最大输出伤害
+		public int maxDamage;
+		/// 品阶
+		public int upgrade;
+
+
+	}
+}
