6d656fb66466ecdb33b91e167e33ce4e47b4da3a
lite <litekok@gmail.com>
2014-05-12 12:35:23 +0800

    道具奖励使用RewardItemObject

---
+++ uc/Assets/TangGame/Scenes/SanguoDemo/RewardItemObject.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace TangGame.UI
+{
+  public class RewardItemObject : MonoBehaviour {
+    public UISprite frame;
+    public UISprite icon;
+
+    private RewardItemData item;
+
+    public void Start(){
+      UIEventListener.Get (icon.gameObject).onClick += OnItemClicked;
+    }
+
+    public void Refresh(RewardItemData item){
+      icon.spriteName = "374";
+      frame.spriteName = "equip_frame_orange";
+    }
+
+    private void OnItemClicked(GameObject obj){
+      Debug.Log ("OnItemClicked" + item.name );
+    }
+  }
+}
