e216d593b49c423828c00562c55e46089e9711ba
zhong zichang <zhongzichang@gmail.com>
2014-05-12 13:00:43 +0800

    删除MainUI.cs 里面的部分测试代码

--- uc/Assets/TangGame/Scripts/UI/MainUI.cs
+++ uc/Assets/TangGame/Scripts/UI/MainUI.cs
@@ -22,63 +22,40 @@ namespace TangGame
     /// The anchor center or center.
     /// </summary>
     public UIAnchor anchorCoC;
-
     // Use this for initialization
-
     void Start ()
     {
-
+      // 设置当前位置为 home
       PlaceController.Place = Place.home;
 
       if (anchorTR != null) {
-	UIPanelNodeManager mgr = new UIPanelNodeManager (anchorTR);
-	mgr.LazyOpen (TangGame.UIContext.MAIN_POPUP_PANEL_NAME, UIPanelNode.OpenMode.OVERRIDE);
-	UIContext.mgrTR = mgr;
+        UIPanelNodeManager mgr = new UIPanelNodeManager (anchorTR);
+        mgr.LazyOpen (TangGame.UIContext.MAIN_POPUP_PANEL_NAME, UIPanelNode.OpenMode.OVERRIDE);
+        UIContext.mgrTR = mgr;
       }
       if (anchorCoC != null) {
-	UIPanelNodeManager mgr = new UIPanelNodeManager (anchorCoC);
-	UIContext.mgrCoC = mgr;
+        UIPanelNodeManager mgr = new UIPanelNodeManager (anchorCoC);
+        UIContext.mgrCoC = mgr;
       }
       if (anchorTL != null) {
-	UIPanelNodeManager mgr = new UIPanelNodeManager (anchorTL);
-	UIContext.mgrTL = mgr;
+        UIPanelNodeManager mgr = new UIPanelNodeManager (anchorTL);
+        UIContext.mgrTL = mgr;
       }
       if (anchorToT != null) {
-	UIPanelNodeManager mgr = new UIPanelNodeManager (anchorToT);
-	mgr.LazyOpen (TangGame.UIContext.MAIN_STATUS_PANEL_NAME, UIPanelNode.OpenMode.OVERRIDE);
-	UIContext.mgrToT = mgr;
+        UIPanelNodeManager mgr = new UIPanelNodeManager (anchorToT);
+        mgr.LazyOpen (TangGame.UIContext.MAIN_STATUS_PANEL_NAME, UIPanelNode.OpenMode.OVERRIDE);
+        UIContext.mgrToT = mgr;
       }
-    }
-
-    // Update is called once per frame
-    void Update ()
-    {
-	
     }
 
     void OnGUI ()
     {
-      if (GUI.Button (new Rect (10, 10, 150, 100), "Back")) {
+      if (GUI.Button (new Rect (10, 10, 50, 60), "Back")) {
 
-	if (TangGame.UIContext.mgrCoC != null) {
-	  TangGame.UIContext.mgrCoC.Back ();
-	}
-
-      }
-
-      if (GUI.Button (new Rect (10, 210, 150, 100), "Battle")) {
-
-        //Application.LoadLevel ("LevelDemo");
-	PlaceController.Place = Place.level;
+        if (TangGame.UIContext.mgrCoC != null) {
+          TangGame.UIContext.mgrCoC.Back ();
+        }
 
       }
-
-      //			if (GUI.Button (new Rect (200, 10, 150, 100), "New Main Popup Panel")) {
-      //				if (mgr != null) {
-      //					mgr.LazyOpen (TangGame.UIContext.MAIN_POPUP_PANEL_NAME, UIPanelNode.OpenMode.ADDITIVE, "test param");
-      //				}
-      //			}
-
-    }
   }
 }
