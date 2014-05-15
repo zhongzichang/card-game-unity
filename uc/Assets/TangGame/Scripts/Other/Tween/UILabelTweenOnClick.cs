4c923c4f2882c5febc267d0db31be9aa794c7322
xbhuang <xbhuang1994@gmail.com>
2014-05-15 11:24:10 +0800

    添加label动画效果脚本

---
+++ uc/Assets/TangGame/Scripts/Other/Tween/UILabelTweenOnClick.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UILabelTweenOnClick : MonoBehaviour
+{
+	public Vector3 labVector = new Vector3 (0, -2, 0);
+	bool mOnClicked;
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnPress (bool bl)
+	{
+		UILabel[] labs = GetComponentsInChildren<UILabel> ();
+		if (bl) {
+			foreach (UILabel lab in labs) {
+				lab.gameObject.transform.localPosition += labVector;
+			}
+		} else {
+			foreach (UILabel lab in labs) {
+				lab.gameObject.transform.localPosition -= labVector;
+			}
+		}
+	}
+}
