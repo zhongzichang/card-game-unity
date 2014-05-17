14edf9ba26ce3ab11e37905f3e8b84878b3418fd
lite <litekok@gmail.com>
2014-05-17 12:48:18 +0800

    为Battle场景添加测试代码，适配1024x615

---
+++ uc/Assets/Test/BattlePve/FixedAspectCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixedAspectCamera : MonoBehaviour {
+
+  void Awake(){
+
+    if (camera != null) {
+
+      Debug.Log("dddffff  "+camera.aspect);
+      camera.aspect = 1024 / 615f;
+      Debug.Log("dddffff dddfffff "+camera.aspect+"   "+Screen.width+"   "+Screen.height);
+    }
+  }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
