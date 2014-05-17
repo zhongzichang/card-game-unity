ceff438f331d795a76c43a160c360f98f15b59aa
lite <litekok@gmail.com>
2014-05-18 00:27:19 +0800

    5月17日发布版本,同步master

--- uc/Assets/TangGame/Scenes/SanguoDemo/InputController.cs
+++ uc/Assets/TangGame/Scenes/SanguoDemo/InputController.cs
@@ -14,11 +14,12 @@ namespace TangGame
 
 		void OnDrag (DragGesture gesture)
 		{
-//    Debug.Log ("DeltaMove" + gesture.DeltaMove);
-//    Debug.Log ("TotalMove" + gesture.TotalMove);
-//    Debug.Log ("Position" + gesture.Position);
+			if (UICamera.hoveredObject != null) {
+				return;
+			}
+
 			float new_pos = grassLayer.transform.localPosition.x + gesture.DeltaMove.x * dragScale;
-			if (new_pos >= -10.24 && new_pos <= 0) {
+      if (new_pos > -10 && new_pos < 0.1) {
 				skyLayer.transform.Translate (Vector3.right * (gesture.DeltaMove.x * dragScale / 4));
 				mountainLayer.transform.Translate (Vector3.right * (gesture.DeltaMove.x * dragScale / 2));
 				grassLayer.transform.Translate (Vector3.right * gesture.DeltaMove.x * dragScale);
