139a345daf286f5bb9a36943ece6406e12435b06
xbhuang <xbhuang1994@gmail.com>
2014-05-22 11:58:34 +0800

    提交许褚的技能

---
+++ uc/Assets/TangLevel/Bhvr/Special/Effector/Xuchu/XuchuQianjinzhui.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TangLevel
+{
+	public class XuchuQianjinzhui : EffectorSpecialBhvr
+	{
+		public Vector3 offset = Vector3.zero;
+		private Transform myTransform;
+		private Transform targetTransform;
+		private Vector3 backupPos;
+		//
+		// Use this for initialization
+		void Awake ()
+		{
+			myTransform = transform;
+		}
+		// Update is called once per frame
+		void Update ()
+		{
+
+			if (isPlay) {
+
+				if (offset.y >= 0) {
+					targetTransform.localPosition = backupPos + offset;
+				} else {
+
+					Release ();
+				}
+			}
+		}
+
+		void OnEnable ()
+		{
+			if (!isPlay) {
+				Resume ();
+			}
+		}
+
+		void mHit ()
+		{
+			base.Hit ();
+		}
+
+		public override void Play ()
+		{
+
+			isPlay = true;
+
+			// 找最近的目标
+			GameObject target = HeroSelector.FindClosestTarget (w.source.GetComponent<HeroBhvr> ());
+			if (target != null) {
+
+			} else {
+				Release ();
+			}
+
+		}
+	}
+}
