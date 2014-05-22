75f39b5d0ccc5992004573e3f5bfb3415eb81b8c
hgq <hgq@temp.com>
2014-05-22 19:42:08 +0800

    1.关卡配置表，怪物配置表解析 2.添加怪物TIPS 3.调整道具TIPS 4.修改了关卡信息界面

--- uc/Assets/TangGame/Scripts/Cache/LevelCache.cs
+++ uc/Assets/TangGame/Scripts/Cache/LevelCache.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TangGame.Xml;
 
 namespace TangGame.UI{
 
@@ -21,5 +22,20 @@ namespace TangGame.UI{
 		public Dictionary<int, Level> levels = new Dictionary<int, Level>();
 
 		
+    /// 获取怪物数据
+    public MonsterData GetMonsterData(int id){
+      if(Config.monsterXmlTable.ContainsKey(id)){
+        return Config.monsterXmlTable[id];
+      }
+      return null;
+    }
+
+    /// 获取关卡数据
+    public Level GetLevel(int id){
+      if(levels.ContainsKey(id)){
+        return levels[id];
+      }
+      return null;
+    }
 	}
 }
