335014a5c1b6ddcd63baef10a47c34c20a2f2c89
xbhuang <xbhuang1994@gmail.com>
2014-05-22 15:36:50 +0800

    完成许褚千斤坠特效

--- uc/Assets/TangLevel/Bhvr/Special/Effector/Xuchu/XuchuQianjinzhui.cs
+++ uc/Assets/TangLevel/Bhvr/Special/Effector/Xuchu/XuchuQianjinzhui.cs
@@ -1,60 +1,78 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace TangLevel
 {
 	public class XuchuQianjinzhui : EffectorSpecialBhvr
 	{
-		public Vector3 offset = Vector3.zero;
-		private Transform myTransform;
-		private Transform targetTransform;
-		private Vector3 backupPos;
+		private Animator mAnimator;
 		//
 		// Use this for initialization
 		void Awake ()
 		{
-			myTransform = transform;
+			mAnimator = GetComponent<Animator> ();
 		}
 		// Update is called once per frame
 		void Update ()
 		{
-
 			if (isPlay) {
-
-				if (offset.y >= 0) {
-					targetTransform.localPosition = backupPos + offset;
-				} else {
-
-					Release ();
-				}
+				mAnimator.enabled = true;
+			} else {
+				mAnimator.enabled = false;
 			}
+
+		}
+
+		void mRelease (){
+			isPlay = false;
+			StartRelease ();
 		}
 
 		void OnEnable ()
 		{
-			if (!isPlay) {
-				Resume ();
+			// 关卡控制
+			LevelController.RaisePause += OnPause;
+			LevelController.RaiseResume += OnResume;
+		}
+
+		void OnDisable ()
+		{
+			// 关卡控制
+			LevelController.RaisePause -= OnPause;
+			LevelController.RaiseResume -= OnResume;
+		}
+		void OnGUI(){
+			if (GUILayout.Button ("Play")) {
+				isPlay = true;
 			}
 		}
 
-		void mHit ()
+		void mCast ()
 		{
-			base.Hit ();
+			if (w == null || w.source == null)
+				return;
+			HeroBhvr sourceHeroBhvr = w.source.GetComponent<HeroBhvr> ();
+			SkillBhvr sourceSkillBhvr = w.source.GetComponent<SkillBhvr> ();
+			// 对方全部活着的英雄
+			List<GameObject> gl = sourceHeroBhvr.hero.battleDirection == BattleDirection.RIGHT ? 
+				LevelContext.AliveEnemyGobjs : LevelContext.AliveSelfGobjs;
+			gl = HeroSelector.FindTargetsWithWidth (gl, transform.position, 3F);
+			foreach (GameObject g in gl) {
+				// 判断对手状态，没在放大招或者处于眩晕状态
+				HeroStatusBhvr targetStatusBhvr = g.GetComponent<HeroStatusBhvr> ();
+				if (targetStatusBhvr.Status != HeroStatus.vertigo && !targetStatusBhvr.IsBigMove) {
+					// 抛出作用器
+					foreach (Effector e in w.effector.subEffectors) {
+						EffectorWrapper cw = EffectorWrapper.W (e, w.skill, w.source, g);
+						sourceSkillBhvr.Cast (cw);
+					}
+				}
+			}
 		}
-
 		public override void Play ()
 		{
-
 			isPlay = true;
-
-			// 找最近的目标
-			GameObject target = HeroSelector.FindClosestTarget (w.source.GetComponent<HeroBhvr> ());
-			if (target != null) {
-
-			} else {
-				Release ();
-			}
-
 		}
 	}
 }
