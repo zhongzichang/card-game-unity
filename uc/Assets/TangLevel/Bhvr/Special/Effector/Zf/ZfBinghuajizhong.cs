5f361ef2fba41874d1db5eabe511ec78b0438a65
zhong zichang <zhongzichang@gmail.com>
2014-05-22 12:21:10 +0800

    补充特效文件

---
+++ uc/Assets/TangLevel/Bhvr/Special/Effector/Zf/ZfBinghuajizhong.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Uni2DLab;
+using UnityEngine;
+
+namespace TangLevel
+{
+  public class ZfBinghuajizhong : EffectorSpecialBhvr
+  {
+    public static Vector3 OFFSET = new Vector3 (0, 1.5F, 0);
+
+    private Transform myTransform;
+
+    void Awake ()
+    {
+      myTransform = transform;
+      animator = GetComponent<Animator> ();
+    }
+
+    void OnEnable ()
+    {
+
+      if (w != null && w.target != null) {
+
+        // 绑定到目标身上
+        myTransform.localPosition = w.target.transform.localPosition + OFFSET;
+        Hit ();
+
+      } else {
+        Release ();
+      }
+
+      // 关卡控制
+      LevelController.RaisePause += OnPause;
+      LevelController.RaiseResume += OnResume;
+
+      StartCoroutine (PlayOnce ("isPlay"));
+
+    }
+
+    void OnDisable ()
+    {
+
+      // 关卡控制
+      LevelController.RaisePause -= OnPause;
+      LevelController.RaiseResume -= OnResume;
+
+    }
+
+    private void OnAnimationEnd ()
+    {
+      Release ();
+    }
+
+    public override void Play ()
+    {
+      isPlay = true;
+    }
+
+    public override void Pause ()
+    {
+      isPlay = false;
+      animator.speed = 10;
+    }
+
+    public override void Resume ()
+    {
+      isPlay = true;
+      animator.speed = 1;
+    }
+  }
+}
+
