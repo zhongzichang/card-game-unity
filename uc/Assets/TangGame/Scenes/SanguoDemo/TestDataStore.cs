2fe0707cb0ec93f8b29715869da6ebb4d49a1e12
xbhuang <xbhuang1994@gmail.com>
2014-05-26 18:22:25 +0800

    解决未装备，可装备，可合成等状态显示不对，可能是没有服务器的原因。

--- uc/Assets/TangGame/Scenes/SanguoDemo/TestDataStore.cs
+++ uc/Assets/TangGame/Scenes/SanguoDemo/TestDataStore.cs
@@ -28,7 +28,7 @@ namespace TangGame.UI
       props.data = new TangGame.Xml.PropsData();
       props.data.name = "测试道具";
       props.data.type = 2;
-      props.count = 2;
+			props.net.count = 2;
       props.data.icon = "104";
       props.data.level = 5;
       props.data.info = "使用后可以获得一个小萝莉";
--- uc/Assets/TangGame/Scripts/UI/Widgets/EquipItem.cs
+++ uc/Assets/TangGame/Scripts/UI/Widgets/EquipItem.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using TangGame.UI;
 using TangGame.Xml;
 using TangGame.Net;
@@ -16,6 +17,14 @@ namespace TangGame.UI
 		public UILabel label;
 		HeroBase hero;
 		Equip equipBase = null;
+		/// <summary>
+		/// 等级满足
+		/// </summary>
+		bool lvMeet;
+		/// <summary>
+		/// 是否拥有装备或可合成装备
+		/// </summary>
+		bool hasEquip;
 
 		public void Flush (HeroBase hero)
 		{
@@ -25,10 +34,8 @@ namespace TangGame.UI
 			if (hero.Net.equipList != null && hero.EquipBases.Length >= index) {
 				equipBase = hero.EquipBases [index - 1];
 			}
-			this.starList.countMax = 15;
-			this.starList.cellWidth = 15;
-			this.starList.cellHeight = 12;
-			this.starList.showBackground = false;
+			this.ContrastLevels ();
+			this.ResetStarList ();
 			if (equipBase.net != null) {
 				this.equipIcon.color = Color.white;
 				this.equipIcon.spriteName = equipBase.data.icon;
@@ -39,14 +46,73 @@ namespace TangGame.UI
 			} else {
 				if (PropsCache.instance.propsTable.ContainsKey (equipBase.data.id)) {
 					this.label.text = UIPanelLang.NOT_EQUIPPED;
-				} else {
+					hasEquip = true;
+				} else if (CheckSynthesis (equipBase.data.id, 1)) {
+					hasEquip = true;
+					this.label.text = UIPanelLang.CAN_SYNTHETIC;
+				}else {
+					hasEquip = false;
 					this.label.text = "";
 				}
-				this.equipIcon.color = new Color32 (0,255,255,255);
+
+				if (lvMeet) {
+					//TODO add 图标为绿色
+				} else {
+					//TODO add 图标为灰色
+				}
+				this.equipIcon.color = new Color32 (0, 255, 255, 255);
 				this.equipIcon.spriteName = equipBase.data.icon;
 				this.equipFrames.spriteName = Global.GetPropFrameName (1);
 				this.starList.count = 0;
 				this.starList.Flush ();
+			}
+		}
+
+
+		bool CheckSynthesis (int propsId, int count)
+		{
+			PropsData propsData = Config.propsXmlTable [propsId];
+			Dictionary<int,int> syntheticPropsTable = propsData.GetSyntheticPropsTable ();
+			if (syntheticPropsTable == null || syntheticPropsTable.Count == 0) {
+				return false;
+			}
+			foreach (KeyValuePair<int,int> propsKeyVal in syntheticPropsTable) {
+				int num = this.CheckContains (propsKeyVal.Key, propsKeyVal.Value * count);
+				if (num < 0) {
+					if (CheckSynthesis (propsKeyVal.Key, count))
+						return false;
+				}
+			}
+			return true;
+		}
+
+		int CheckContains (int propsId, int count)
+		{
+			if (PropsCache.instance.propsTable.ContainsKey (propsId)) {
+				int num = PropsCache.instance.propsTable [propsId].net.count - count;
+				return num;
+			} else {
+				return -1;
+			}
+		}
+
+		void ResetStarList ()
+		{
+			this.starList.countMax = 15;
+			this.starList.cellWidth = 15;
+			this.starList.cellHeight = 12;
+			this.starList.showBackground = false;
+		}
+
+		/// <summary>
+		/// 检查等级
+		/// </summary>
+		public void ContrastLevels ()
+		{
+			if (equipBase.data.level <= hero.Net.level) {
+				lvMeet = true;
+			} else {
+				lvMeet = false;
 			}
 		}
 
@@ -60,6 +126,19 @@ namespace TangGame.UI
 			} else {
 				bean.props = equipBase; 
 				TangGame.UIContext.mgrCoC.LazyOpen (UIContext.EQUIP_INFO_PANEL_NAME, UIPanelNode.OpenMode.ADDITIVE, UIPanelNode.BlockMode.SPRITE, bean);
+			}
+		}
+
+		/// <summary>
+		/// 可以穿戴
+		/// </summary>
+		public bool CanWear {
+			get {
+				if (hasEquip && lvMeet) {
+					return true;
+				} else {
+					return false;
+				}
 			}
 		}
 	}
