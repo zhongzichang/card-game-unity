732f2d723f69aa54999349cd4bff4c138030cc65
zhong zichang <zhongzichang@gmail.com>
2014-05-28 16:54:51 +0800

    神力蓄力特效，大招保留在原处

--- uc/Assets/TangLevel/Bhvr/Hero/BigMoveBhvr.cs
+++ uc/Assets/TangLevel/Bhvr/Hero/BigMoveBhvr.cs
@@ -7,30 +7,42 @@ using DragonBones;
 
 namespace TangLevel
 {
+  [RequireComponent (typeof(MaterialBhvr))]
   public class BigMoveBhvr : MonoBehaviour
   {
-
     // 大招准备
-    public delegate void BigMoveHandler(bool ready);
+    public delegate void BigMoveHandler (bool ready);
     // 大招不可行
     public BigMoveHandler RaiseEvent;
-
-    public static readonly Vector3 OFFSET = new Vector3 (0F, 0F, -100F); // 大招时的位置偏移，需要往镜头靠近
+    public static readonly Vector3 OFFSET = new Vector3 (0F, 0F, 0F);
+    // 大招时的位置偏移，需要往镜头靠近
     public const float SCALE = 1.3F;
+    public static readonly Color COLOR_MASH = new Color (0.5F, 0.5F, 0.5F, 1F);
     private HeroStatusBhvr statusBhvr;
     private TDB.DragonBonesBhvr dbBhvr;
-    private HashSet<BigMoveBhvr> bigMoveSenders = new HashSet<BigMoveBhvr> (); // 接收到的所有大招发送者
-    private bool inited = false; // 该组件是否已初始化
-    private Transform myTransform; // Transform
-    private Vector3 backupPos = Vector3.zero; // 备份的位置
-    private DirectedNavAgent agent; // 导航代理
-    private HeroBhvr heroBhvr; // heroBhvr
-    private Vector3 backupScale = Vector3.zero; // 大招前的人物比例
-    private Armature armature; // Dragonbones armature
-    private Skill skill; // 大招对应的技能
-    private bool ready = false; // 大招是否准备好
+    private HashSet<BigMoveBhvr> bigMoveSenders = new HashSet<BigMoveBhvr> ();
+    // 接收到的所有大招发送者
+    private bool inited = false;
+    // 该组件是否已初始化
+    private Transform myTransform;
+    // Transform
+    private Vector3 backupPos = Vector3.zero;
+    // 备份的位置
+    private DirectedNavAgent agent;
+    // 导航代理
+    private HeroBhvr heroBhvr;
+    // heroBhvr
+    private Vector3 backupScale = Vector3.zero;
+    // 大招前的人物比例
+    private Armature armature;
+    // Dragonbones armature
+    private Skill skill;
+    // 大招对应的技能
+    private bool ready = false;
+    // 大招是否准备好
+    private MaterialBhvr matBhvr;
 
-#region MonoMethods
+    #region MonoMethods
 
     void Start ()
     {
@@ -47,34 +59,29 @@ namespace TangLevel
       // 获取大招技能
       skill = heroBhvr.BigMoveSkill ();
 
+      matBhvr = GetComponent<MaterialBhvr> ();
+
     }
 
     void Update ()
     {
-      if( IsBigMoveReady() )
-        {
-          // 可以施放大招，点亮大招按钮
-          if( !ready )
-            {
-              ready = true;
-              if( RaiseEvent != null )
-                {
-                  RaiseEvent(ready);
-                }
-            }
+      if (IsBigMoveReady ()) {
+        // 可以施放大招，点亮大招按钮
+        if (!ready) {
+          ready = true;
+          if (RaiseEvent != null) {
+            RaiseEvent (ready);
+          }
         }
-      else
-        {
-          // 熄灭大招按钮
-          if( ready )
-            {
-              ready = false;
-              if( RaiseEvent != null )
-                {
-                  RaiseEvent(ready);
-                }
-            }
+      } else {
+        // 熄灭大招按钮
+        if (ready) {
+          ready = false;
+          if (RaiseEvent != null) {
+            RaiseEvent (ready);
+          }
         }
+      }
     }
 
     void OnEnable ()
@@ -116,9 +123,9 @@ namespace TangLevel
       }
     }
 
-#endregion
+    #endregion
 
-#region SceneEvents
+    #region SceneEvents
 
     /// <summary>
     /// 大招开始
@@ -127,10 +134,7 @@ namespace TangLevel
     /// <param name="args">Arguments.</param>
     private void OnBigMoveStart (object sender, EventArgs args)
     {
-      Debug.Log("OnBigMoveStart");
-      if (!statusBhvr.IsBigMove) { // 本人没有放大招
-        statusBhvr.IsPause = true;
-      }
+      Lock ();
     }
 
     /// <summary>
@@ -140,15 +144,12 @@ namespace TangLevel
     /// <param name="args">Arguments.</param>
     private void OnBigMoveEnd (object sender, EventArgs args)
     {
-      Debug.Log("OnBigMoveEnd");
-      if (statusBhvr.IsPause) {
-        statusBhvr.IsPause = false;
-      }
+      BreakLock ();
     }
 
-#endregion
+    #endregion
 
-#region DragonBonesEvents
+    #region DragonBonesEvents
 
     private void OnAnimationFrameEvent (Com.Viperstudio.Events.Event e)
     {
@@ -164,9 +165,9 @@ namespace TangLevel
       }
     }
 
-#endregion
+    #endregion
 
-#region PublicMethods
+    #region PublicMethods
 
     /// <summary>
     /// 开始施放大招
@@ -208,9 +209,31 @@ namespace TangLevel
 
     }
 
-#endregion
+    /// <summary>
+    /// 大招锁定
+    /// </summary>
+    public void Lock(){
 
-#region PrivateMethods
+      if (!statusBhvr.IsBigMove) { // 本人没有放大招
+        statusBhvr.IsPause = true;
+        matBhvr.color = matBhvr.color * COLOR_MASH;
+      }
+    }
+
+    /// <summary>
+    /// 解除大招锁定
+    /// </summary>
+    public void BreakLock ()
+    {
+      if (statusBhvr.IsPause) {
+        statusBhvr.IsPause = false;
+        matBhvr.RestoreColor ();
+      }
+    }
+
+    #endregion
+
+    #region PrivateMethods
 
     private bool IsBigMoveReady ()
     {
@@ -260,9 +283,9 @@ namespace TangLevel
         case Skill.TARGET_REGION:
           // 指定区域
           Rect region = skill.region;
-          Vector2 center = new Vector2(region.x - region.width/2, region.y - region.height/2);
-          if( Mathf.Abs (myTransform.localPosition.x - center.x)
-              <= skill.distance){
+          Vector2 center = new Vector2 (region.x - region.width / 2, region.y - region.height / 2);
+          if (Mathf.Abs (myTransform.localPosition.x - center.x)
+             <= skill.distance) {
             return true;
           }
           break;
@@ -271,7 +294,7 @@ namespace TangLevel
       return false;
     }
 
-#endregion
+    #endregion
   }
 }
 
---
+++ uc/Assets/TangLevel/Bhvr/Hero/MaterialBhvr.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UnityEngine;
+
+namespace TangLevel
+{
+  public class MaterialBhvr : MonoBehaviour
+  {
+    private Renderer[] renderers;
+    private Material[] sourceMats;
+    private Color[] colors;
+    private static readonly Color DEFAULT_COLOR = Color.white;
+    private Color mColor = DEFAULT_COLOR;
+
+    #region Properties
+
+    public Color color {
+      get {
+        return mColor;
+      }
+      set {
+        mColor = value;
+        for (int i = 0; i < sourceMats.Length; i++) {
+          sourceMats [i].color = mColor;
+        }
+      }
+    }
+
+    #endregion
+
+    #region MonoMethods
+
+    void OnEnable ()
+    {
+      if (sourceMats == null) {
+        renderers = GetComponentsInChildren<Renderer> ();
+        sourceMats = new Material[renderers.Length];
+        colors = new Color[renderers.Length];
+        for (int i = 0; i < renderers.Length; i++) {
+          sourceMats [i] = renderers [i].material;
+          colors [i] = renderers [i].material.color;
+        }
+      }
+    }
+
+    void OnDisable ()
+    {
+      RestoreColor ();
+    }
+
+    #endregion
+
+    #region PublicMethods
+
+    /// <summary>
+    /// 还原颜色
+    /// </summary>
+    public void RestoreColor ()
+    {
+      if (renderers != null && sourceMats != null && colors != null) {
+        for (int i = 0; i < renderers.Length && i < sourceMats.Length; i++) {
+          sourceMats [i].color = colors [i];
+        }
+        mColor = DEFAULT_COLOR;
+      }
+    }
+
+    #endregion
+  }
+}
+
