a8c648086079debd0d22cac18bab3e919c56785e
lite <litekok@gmail.com>
2014-05-28 20:33:10 +0800

    对通讯API做点重构

--- uc/Assets/Test/HttpClientTest/ClientDemo.cs
+++ uc/Assets/Test/HttpClientTest/ClientDemo.cs
@@ -7,9 +7,10 @@ namespace ClientDemoTest{
 
   public class ClientDemo : MonoBehaviour {
 
-    private LoginService loginService = new LoginService ();
-    private HeroService heroService = new HeroService ();
-    private HttpsTestService httpsTestService = new HttpsTestService();
+    private LoginApi loginApi = new LoginApi ();
+    private UserApi userApi = new UserApi ();
+    private HeroApi heroApi = new HeroApi ();
+    private TestApi testApi = new TestApi();
 
     private string msg = "...";
 
@@ -17,25 +18,48 @@ namespace ClientDemoTest{
 
       GUILayout.Label (msg);
       if (GUILayout.Button ("login")) {
-        loginService.login("user", "pass", loginResponseHandler);
+        loginApi.login("user", "pass", loginResponseHandler);
       }
       if (GUILayout.Button ("getHeroes")) {
-        loginService.getHeroes ("1", getHeroesResponseHandler);
+        string userId = "1";
+        userApi.getHeroes (userId, getHeroesResponseHandler);
       }
       if (GUILayout.Button ("equipItem")) {
-        heroService.equipItem("", "", equipItemResponseHandler);
+        System.Action<EquipItemResult> equipItemResponseHandler = delegate(EquipItemResult result){
+          Debug.Log(result.ok);
+        };
+        heroApi.equipItem("heroId", "equipId", equipItemResponseHandler);
       }
+      if (GUILayout.Button ("upgradeRank")) {
+        System.Action<UpgradeRankResult> upgradeRankResponseHandler = delegate(UpgradeRankResult result){
+          Debug.Log(result.ok);
+        };
+        heroApi.upgradeRank("heroId", upgradeRankResponseHandler);
+      }
+      if (GUILayout.Button ("upgradeStar")) {
+        System.Action<UpgradeStarResult> upgradeStarResponseHandler = delegate(UpgradeStarResult result){
+          Debug.Log(result.ok);
+        };
+        heroApi.upgradeStar("heroId", upgradeStarResponseHandler);
+      }
+      if (GUILayout.Button ("upgradeSkill")) {
+        System.Action<UpgradeSkillResult> upgradeSkillResponseHandler = delegate(UpgradeSkillResult result){
+          Debug.Log(result.ok);
+        };
+        heroApi.upgradeSkill("heroId", "skillId", upgradeSkillResponseHandler);
+      }
+
       if (GUILayout.Button ("httpsGithub")) {
         msg = "";
-        httpsTestService.httpsGithub (githubResponseHandler);
+        testApi.httpsGithub (githubResponseHandler);
       }
       if (GUILayout.Button ("httpsLocal")) {
         msg = "";
-        httpsTestService.httpsLocal (githubResponseHandler);
+        testApi.httpsLocal (githubResponseHandler);
       }
       if (GUILayout.Button ("httpLocal")) {
         msg = "";
-        httpsTestService.httpLocal (githubResponseHandler);
+        testApi.httpLocal (githubResponseHandler);
       }
   	}
 
@@ -47,20 +71,15 @@ namespace ClientDemoTest{
 
     private void loginResponseHandler (LoginResult result)
     {
-      Debug.Log (result.name);
+      Debug.Log (result.userId);
     }
 
     private void getHeroesResponseHandler (HeroResult result)
     {
-      HeroNet[] heroes = (HeroNet[])result.data;
-      foreach (HeroNet hero in heroes) {
+      HeroNetItem[] heroes = (HeroNetItem[])result.data;
+      foreach (HeroNetItem hero in heroes) {
         Debug.Log (hero.id);
       }
-    }
-
-    private void equipItemResponseHandler (EquipItemResult result)
-    {
-      Debug.Log (result.name);
     }
 
   }
