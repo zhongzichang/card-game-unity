e0ce2baede8c2636dbba7be46a6d1849be6d9ccd
zhong zichang <zhongzichang@gmail.com>
2014-05-29 17:08:45 +0800

    修正眩晕无效的问题

--- uc/Assets/TangLevel/Bhvr/Hero/HeroStatusBhvr.cs
+++ uc/Assets/TangLevel/Bhvr/Hero/HeroStatusBhvr.cs
@@ -6,15 +6,18 @@ namespace TangLevel
   public class HeroStatusBhvr : MonoBehaviour
   {
     #region delegates
+
     public delegate void StatusChange (HeroStatus status);
 
     public delegate void PauseChange (bool pause);
     // 状态结束时回调
     public StatusChange statusChangedHandler;
     public PauseChange pauseChangedHandler;
+
     #endregion
 
     #region attributes
+
     /// <summary>
     ///   角色状态，在*inspector*中使用
     /// </summary>
@@ -27,10 +30,11 @@ namespace TangLevel
     public bool m_isPause = false;
     public bool newIsPause = false;
     public bool m_isBigMove = false;
+
     #endregion
 
-
     #region properties
+
     /// <summary>
     ///   角色状态，被脚本使用，暂停的情况下不能修改状态
     /// </summary>
@@ -39,12 +43,11 @@ namespace TangLevel
         return m_status;
       }
       set {
-        if (m_status != HeroStatus.dead// 死亡后不能修改状态
-            && !m_isBigMove// 大招的时候不能修改状态
+        if (m_status != HeroStatus.dead // 死亡后不能修改状态
+          && !m_isBigMove // 大招的时候不能修改状态
           && newStatus != value // 状态相同不做修改
+          && newStatus != HeroStatus.vertigo // 已经改为眩晕状态，暂时就不能修改成其他状态，需要等眩晕生效后才能修改
         ) {
-
-
           switch (m_status) {
           // 眩晕状态下只能改为空闲
           case HeroStatus.vertigo:
@@ -56,13 +59,10 @@ namespace TangLevel
             newStatus = value;
             break;
           }
-
-          
         }
       }
     }
     // 暂停 ----
-
     public bool IsPause {
       get {
         return m_isPause;
@@ -88,15 +88,17 @@ namespace TangLevel
         m_isBigMove = value;
       }
     }
+
     #endregion
 
     #region monomethods
+
     void Update ()
     {
 
       // 暂停改变通知
       if (m_isPause != newIsPause) {
-        if( newIsPause && IsBigMove ){
+        if (newIsPause && IsBigMove) {
           Debug.Log ("pause when bigmove");
         }
         m_isPause = newIsPause;
@@ -106,9 +108,10 @@ namespace TangLevel
       }
 
       // 状态改变通知
-      if (m_status != newStatus) {
+      if (newStatus != HeroStatus.none && m_status != newStatus) {
         beforeStatus = m_status; // 保留状态
         m_status = newStatus; // 切换到新状态
+        newStatus = HeroStatus.none; // 新状态设置为 none
         if (statusChangedHandler != null) {
           statusChangedHandler (m_status);
         }
