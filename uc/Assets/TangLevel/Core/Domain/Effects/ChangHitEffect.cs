2dd699bd190384d1f792fea884d3826f24472320
zhong zichang <zhongzichang@gmail.com>
2014-06-04 11:12:10 +0800

    增加效果

---
+++ uc/Assets/TangLevel/Core/Domain/Effects/ChangHitEffect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TangLevel
+{
+  /*
+   * 普通攻击默认命中率为100%
+改变命中后，普通攻击命中率＝（命中值－闪避值）／100，命中率大于1表示一定会命中。
+技能命中率见做作用器的概率属性。*/
+  public class ChangHitEffect : Effect
+  {
+    public const int TYPE = 24;
+    public override void Arise ()
+    {
+    }
+  }
+}
+
---
+++ uc/Assets/TangLevel/Core/Domain/Effects/ChargeHurtEffect.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TangLevel
+{
+  /*
+   * 将前摇时间作为参数，进入伤害运算，时间越长，伤害越高。
+{伤害作用器编号，系数}与另一个纯粹用作伤害的作用器配合使用。表示在该作用器产生伤害时额外乘以一个系数。
+加成系数＝(前摇时间(毫秒)／1000)＊系数
+魔法伤害＝(魔法强度＊技能系数＋升级提升参数＊技能等级)*加成系数*/
+  public class ChargeHurtEffect : Effect
+  {
+    public const int TYPE = 27;
+
+    public override void Arise ()
+    {
+    }
+  }
+}
+
