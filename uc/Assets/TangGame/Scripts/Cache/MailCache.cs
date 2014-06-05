f026c68f79b143fbebee962785d73e507dd9f3e8
hgq <hgq@temp.com>
2014-06-05 17:11:21 +0800

    1.邮箱相关功能

---
+++ uc/Assets/TangGame/Scripts/Cache/MailCache.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TangGame.UI{
+  public class MailCache  {
+    
+    private static MailCache mInstance;
+    
+    public static MailCache instance {
+      get{
+        if(null == mInstance){
+          mInstance = new  MailCache();
+        }
+        return mInstance; 
+      }
+    }
+
+    public List<Mail> list = new List<Mail>();
+
+    /// List排序使用
+    private int SortLevel(Mail a1, Mail a2){
+      return a1.time.CompareTo(a2.time);
+    }
+  }
+}
