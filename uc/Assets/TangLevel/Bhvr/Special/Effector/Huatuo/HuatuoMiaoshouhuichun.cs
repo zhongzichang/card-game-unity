a72472599e1f9486df090b08a591305646735b9a
xbhuang <xbhuang1994@gmail.com>
2014-06-05 15:26:23 +0800

    调整妙手回春技能

--- uc/Assets/TangLevel/Bhvr/Special/Effector/Huatuo/HuatuoMiaoshouhuichun.cs
+++ uc/Assets/TangLevel/Bhvr/Special/Effector/Huatuo/HuatuoMiaoshouhuichun.cs
@@ -6,6 +6,11 @@ namespace TangLevel
 {
 	public class HuatuoMiaoshouhuichun : EffectorSpecialBhvr
 	{
+		/// <summary>
+		/// The skills mount point.
+		/// 技能挂载点
+		/// </summary>
+		public Vector3 skillsMountPoint = new Vector3 (0, 2.5f, 0);
 		// Use this for initialization
 		void Awake ()
 		{
@@ -37,11 +42,11 @@ namespace TangLevel
 		IEnumerator mRelease ()
 		{
 
-			yield return new WaitForSeconds (0.5f);
+			yield return new WaitForSeconds (0.2f);
 			mCast (w.source);
 			mCast (skillTarget);
 			mCast (nextTarget);
-			yield return new WaitForSeconds (0.5f);
+			yield return new WaitForSeconds (0.2f);
 			isPlay = false;
 			transform.parent = null;
 			StartRelease ();
@@ -86,7 +91,7 @@ namespace TangLevel
 
 		public override void Play ()
 		{
-			mCurrent = w.source.transform.position + new Vector3(0,2.5f,0);
+			mCurrent = w.source.transform.position + skillsMountPoint;
 			skillTarget = HeroSelector.FindSelfWeakest (w.source.GetComponent<HeroBhvr> ().hero);
 			if (skillTarget != null) {
 				nextTarget = HeroSelector.FindclosestFriend (skillTarget.GetComponent<HeroBhvr> ());
@@ -99,9 +104,9 @@ namespace TangLevel
 			trailRenderer.time = trailTime;
 			targetVectors.Enqueue (mCurrent);
 			if (skillTarget != null)
-				targetVectors.Enqueue (skillTarget.transform.position + new Vector3(0,2.5f,0));
+				targetVectors.Enqueue (skillTarget.transform.position + skillsMountPoint);
 			if (nextTarget != null)
-				targetVectors.Enqueue (nextTarget.transform.position + new Vector3(0,2.5f,0));
+				targetVectors.Enqueue (nextTarget.transform.position + skillsMountPoint);
 			StartCoroutine (mRelease ());
 
 		}
@@ -123,8 +128,8 @@ namespace TangLevel
 			GameObject obj = NGUITools.AddChild (gameObject, miaoshouhuichun_sub);
 			obj.SetActive (true);
 			obj.transform.parent = null;
-			obj.transform.position += new Vector3 (0, 0, -5);
-			GameObject.Destroy (obj, 0.5f);
+			obj.transform.position += Vector3.back;
+			GameObject.Destroy (obj, 0.3f);
 		}
 	}
 }
