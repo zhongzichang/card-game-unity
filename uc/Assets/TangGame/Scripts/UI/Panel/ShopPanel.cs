2883434654ebcddc10e73ed290beebc2ac26f1a2
hgq <hgq@temp.com>
2014-06-11 19:06:09 +0800

    1.商店面板功能

---
+++ uc/Assets/TangGame/Scripts/UI/Panel/ShopPanel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TangGame.UI{
+  /// <summary>
+  /// 商店面板
+  /// </summary>
+  public class ShopPanel :ViewPanel {
+    
+
+  }
+}
---
+++ uc/Assets/TangGame/Scripts/UI/Widgets/ShopItem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TangGame.UI{
+  public class ShopItem : ViewItem {
+
+    public ViewItemDelegate onClick;
+    public UILabel nameLabel;
+    public UILabel priceLabel;
+    public GameObject ingotIcon;
+    public GameObject moenyIcon;
+    public GameObject mask;
+    public SimplePropsItem props;
+   
+  }
+}
