2c6418477cde5da1c3d0a2d195d783398cc42b59
hgq <hgq@temp.com>
2014-06-23 18:54:34 +0800

    1.进入游戏界面，包括登陆和服务器列表

---
+++ uc/Assets/TangGame/Scripts/Cache/GameCache.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TangGame.UI{
+  /// <summary>
+  /// 游戏数据存储，比如版本号，登陆相关，服务器列表相关
+  /// </summary>
+  public class GameCache {
+
+    private static GameCache mInstance;
+    
+    public static GameCache instance {
+      get {
+        if (null == mInstance) {
+          mInstance = new GameCache ();
+        }
+        return mInstance; 
+      }
+    }
+
+    /// 游戏的字符串版本号
+    public string version;
+    /// 游戏的数字版本号
+    public int versionCode;
+
+  }
+}
