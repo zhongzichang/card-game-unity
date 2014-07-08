0cce805301d7b043bbc7e66730c3161711f4b6f4
hgq <hgq@temp.com>
2014-07-08 17:23:29 +0800

    1.任务领取奖励面板完善

---
+++ uc/Assets/TangGame/Scripts/UI/VO/TaskAwardPanelData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TangGame.UI{
+  public class TaskAwardPanelData  {
+
+    public Task task = null;
+    public Daily daily = null;
+
+  }
+}
