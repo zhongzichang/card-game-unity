ea1d6c3c2224872ef18ea258d447c80411d137b6
hgq <hgq@temp.com>
2014-07-10 15:57:06 +0800

    1.VIP充值界面

---
+++ uc/Assets/TangGame/Scripts/UI/Panel/RechargePanel.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TangGame.UI{
+  /// <summary>
+  /// 充值界面
+  /// </summary>
+  public class RechargePanel : ViewPanel {
+    public UIEventListener btn;
+    public UILabel btnLabel;
+
+    //=================================
+    public GameObject vipBuyGroup;
+    public UIScrollView vipBuyScrollView;
+    public GameObject vipBuyContainer;
+    public VipBuyItem vipBuyItem;
+
+    void Start(){
+      vipBuyItem.gameObject.SetActive(false);
+      btn.onClick += BtnClickHandler;
+      DisplayVipBuyItems();
+    }
+
+
+    private void BtnClickHandler(GameObject go){
+
+    }
+
+    /// 显示VIP的购买项
+    private void DisplayVipBuyItems(){
+      Vector3 tempPosition = vipBuyItem.transform.localPosition;
+      Vector3 temp = Vector3.zero;
+      for(int i = 0; i < 10; i++){
+        GameObject go = UIUtils.Duplicate(vipBuyItem.gameObject, vipBuyContainer);
+        temp.x = tempPosition.x + i % 2 * 410;
+        temp.y = tempPosition.y - (int)(i / 2) * 143;
+        go.transform.localPosition = temp;
+        UIEventListener.Get(go).onClick += ItemClickHandler;
+      }
+    }
+
+    /// VIP购买项点击处理
+    private void ItemClickHandler(GameObject go){
+
+    }
+  }
+}
---
+++ uc/Assets/TangGame/Scripts/UI/Widgets/VipBuyItem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TangGame.UI{
+  /// <summary>
+  /// Vip界面的购买项
+  /// </summary>
+  public class VipBuyItem : ViewItem {
+    
+    public UISprite icon;
+    public UILabel titleLabel;
+    public UILabel moneyLabel;
+    public UILabel descLabel;
+  }
+}
+
